ab89a033533daf8a8f54deec4258e626f5965c9e
DESKTOP-GLTSSF0\DreamMachines <kamila0wozniak@gmail.com>
2022-05-08 21:43:27 +0200

    home page etc

--- src/components/HomePageFirstSection.js
+++ src/components/HomePageFirstSection.js
@@ -8,23 +8,23 @@ export default function homePageFirstSection() {
             <div className={styles.boxInside}>
                 <div className={styles.textSection}>
                     <div className={styles.cube}>
-                        <h4>lsd.studio</h4>
-                        <h5>to zespół kompetentnych specjalistów zajmujących sie projektowaniem i realizacją wnętrz.</h5>
+                        <h5>lsd.studio</h5>
+                        <h4>to zespół kompetentnych specjalistów zajmujących sie projektowaniem i realizacją wnętrz.</h4>
                         <p>Doświadczenie dotyczące funkcjonowania marek wykorzystujemy w przygotowaniu skutecznych strategii projektowych, które w oparciu o aktualne trendy - pozwalają wyprzedzić konkurencję. Wieloletnia obecność na rynku oraz imponująca ilość zrealizowanych projektów – to kapitał, gwarantujący profesjonalną obsługę na każdym etapie realizacji procesu inwestycyjnego.</p>
                     </div>
                     <div>
-                        <h4>lsd.studio</h4>
-                        <h5>to kompleksowa obsługa dowolnej inwestycji.</h5>
+                        <h5>lsd.studio</h5>
+                        <h4>to kompleksowa obsługa dowolnej inwestycji.</h4>
                         <p>Nasze studio oferuje wsparcie podczas całego procesu przebiegu inwestycji: od autorskiego projektu koncepcyjnego, przez wielobranżowy projekt wykonawczy, aż po całościową realizację „pod klucz”. Precyzyjnie przeprowadzony proces projektowy, poprzedzony wnikliwą analizą potrzeb i możliwości klienta, skuteczna realizacja założeń w dowolnym obszarze: od prac budowlanych i instalacyjnych aż po produkcję wyposażenia – wszystko to oferujemy w ramach kompleksowej obsługi inwestycji.</p>
                     </div>
                     <div>
-                        <h4>lsd.studio</h4>
-                        <h5>to gwarancja najwyższej jakości.</h5>
+                        <h5>lsd.studio</h5>
+                        <h4>to gwarancja najwyższej jakości.</h4>
                         <p>Najlepszym potwierdzeniem naszego doświadczenia i wysokich kompetencji są zdobyte nagrody. Nasze projekty wielokrotnie zostały docenione przez specjalistów z branży, zdobywając wiele prestiżowych nagród i wyróżnień.</p>
                     </div>
                 </div>
                 <div className={styles.buttonSection}>
-                    <Button variant="outline-dark">Discover Now</Button>
+                    <Button className={styles.button} variant="outline-dark">Discover Now</Button>
                 </div>
 
             </div>
--- src/components/HomePageSecondSection.js
+++ src/components/HomePageSecondSection.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './HomePageSecondSection.module.css'
+import Button from "react-bootstrap/Button";
 
 export default function HomePageSecondSection() {
     return(
         <div className={styles.background}>
             <div className={styles.section}>1</div>
             <div className={styles.section}>2</div>
-            <div className={styles.section}>3</div>
+            <div className={styles.section}>
+                <div className={styles.buttonSection}>
+                    <Button className={styles.button} variant="outline-dark">Discover Now</Button>
+                </div>
+
+            </div>
         </div>
     )
 }
